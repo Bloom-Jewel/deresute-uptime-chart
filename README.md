096287e0181d0bce5cd37179011cdf002f97ab0d
Rei Hakurei <reimu_after_marisa@yahoo.com>
2018-04-24 08:19:02 +0700

    [ADD] stdin scanner
      this stdin scanner works like C++
      about the program output, just care the last line.
    - [ADD] skill composition table
      works for actual skill uptime table
    - [FIX] perform some generalization
    - [FIX] [TMP] change the ctime into debuggable for now
    - [ADD] allow card shortname and song/chart name for the caption

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # Starlight Stage Uptime Chart Generator
 ## Explanation
   uptime graph of starlight stage skill activation.
+
 ## Restriction
 - does not take count of current HP
 - does not take count maximum power of valued skills such as SU vs SSU (OL) vs SSSU (CC)
+
 ## Examples
 1. `2018 04 22 ver.` Prototype
   ![](https://cdn.discordapp.com/attachments/338119821868662785/437625369835732994/unknown.png)
   cards that used on example can be seen in `debug_uptime.md`
 2. ... and so on.
+
 ## Requirements
 - Ruby 2.3.0
 - Image Magick
--- uptime.rb
+++ uptime.rb
@@ -1,7 +1,12 @@
-require_relative 'scannner'
+require 'json'
+
+# Load non-public global module
+require 'bjp/common'
+# Load separated scanner
+require_relative 'scanner'
 
 module BloomJewel; end
-LiveCard = Struct.new(:id,:interval,:base_uptime,:level,:skill)
+LiveCard = Struct.new(:id,:interval,:base_uptime,:level,:skill,:name)
 class LiveCard
   SKILL_TABLE = {
     SU: [1,2,3],
@@ -37,9 +42,10 @@ class BloomJewel::SkillUptime
     
     @cards.each(&:skill!)
   end
-  def draw(length)
+  def draw(length,songname)
+    p [songname]
     length = [[length.to_f,20.0].max,180.0].min
-    cw,ch = 1366,480
+    cw,ch = 1366,560
     
     img = Magick::Image.new(cw,ch) do self.background_color = 'white' end
     canvas = Magick::Draw.new
@@ -50,12 +56,20 @@ class BloomJewel::SkillUptime
       canvas.stroke('black')
       canvas.fill('none')
       #
+      ss = {}
       rx, ry = 96, 64
       rw, rh = cw - 32, 448
       spx = ->(time){ rx + Rational(time,length) * (rw - rx) }
       sprx = ->(time){ spx.call(time).round }
       rcx = ->(time,uptime){ [time,[time+uptime,length].min].map(&sprx) }
       #
+      skc = {
+        OL: [:SU,:CG],
+        CC: [:SU],
+        FC: [:SU,:CU],
+        AR: [:CU,:HP],
+        SYN:[:SU,:CU,:HP],
+      }
       {
         SU: '#FF8040',
         CU: '#EEFF60',
@@ -64,7 +78,7 @@ class BloomJewel::SkillUptime
         PL: '#60FFCC',
         CG: '#778830',
         DG: '#30EEFF',
-        LR: [:HP],
+        LR: [:HP], # undecided placeholder
         MM: '#CC33FF',
         SB: '#FF3333',
         
@@ -77,45 +91,90 @@ class BloomJewel::SkillUptime
       }.tap do |skills|
         cardi = 1
         cardn = @cards.size + 1
-        @cards.each do |card|
-          colors = []
-          case skills[card.skill]
+        # -- UPCARD / CARD UPTIME --
+        getc = ->(skill){
+          case skills[skill]
           when String
-            colors << skills[card.skill]
+            [skills[skill]]
           when Array
-            colors.concat skills.select{|k| skills[card.skill].include?(k) }.values
+            skills.select{|k| skills[skill].include?(k) }.values
           else
-            colors << 'gray'
+            ['gray']
           end
+        }
+        dskill = ->(co,ci,cn,x1,x2,ym,yl,yh){
+          yc    = (yl + Rational(ci,cn) * yh).round
+          ya,yb = yc - ym, yc + ym
+          yn = co.size
+          co.size.times do |yi|
+            y1,y2 = [yi,yi+1].map{|yx|ya + Rational(yx,yn)*(yb-ya)}.map(&:round)
+            color = co[yi]
+            canvas.stroke('none')
+            canvas.fill(color)
+            canvas.rectangle(x1,y1,x2,y2)
+          end
+        }
+        @cards.each do |card|
+          colors = getc.call(card.skill)
           card.interval.step(length,card.interval) do |utime|
-            x1,x2 = rcx.call(utime,card.uptime)
-            yc,ym = (ry + Rational(cardi,cardn) * (rh - ry)).round, 12
-            ya,yb = yc - ym, yc + ym
-            yn = colors.size
-            colors.size.times do |yi|
-              y1,y2 = [yi,yi+1].map{|yx|ya + Rational(yx,yn)*(yb-ya)}.map(&:round)
-              color = colors[yi]
-              canvas.stroke('none')
-              canvas.fill(color)
-              canvas.rectangle(x1,y1,x2,y2)
+            next if utime > @notes.max
+            skc.fetch(card.skill,[card.skill]).each do |skpure|
+              ss[skpure] ||= []
+              ss[skpure] << [utime,utime + card.uptime]
             end
+            x1,x2 = rcx.call(utime,card.uptime)
+            dskill.call(colors,cardi,cardn,x1,x2,12,ry,rh-ry)
+          end
+          cardi += 1
+        end
+        # -- UPSKILL / OVERALL SKILL UPTIME
+        ss.each do |sk,sv|
+          sv.uniq!
+          sv.sort!
+          #sv.sort_by! do |(sa,sb)| [sa,sb] end
+          si,sj = 0,0
+          while si < sv.size
+            sj = si + 1
+            break if sj >= sv.size
+            s1,s2 = sv[si],sv[sj]
+            m1,m2 = s1.max,s2.max
+            mx = s1 + s2
+            if mx != mx.sort || mx != mx.uniq then
+               sv.delete_at(sj)
+              s1.replace([mx.min,mx.max])
+            else
+              si += 1
+            end
+          end
+        end
+        canvas.stroke('none')
+        cardi = 1
+        cardn = ss.size
+        cwh = ch-(rh+32)
+        cws = Rational(cwh,2 * ss.size)
+        ss.each do |skill,suptimes|
+          colors = getc.call(skill)
+          suptimes.each do |u1,u2|
+            x1,x2 = rcx.call(u1,u2-u1)
+            dskill.call(colors,cardi,cardn,x1,x2,cws,rh - cws,cwh)
           end
           cardi += 1
         end
       end
       #
       canvas.stroke_width(1)
-      canvas.stroke('black')
       canvas.fill('none')
-      canvas.rectangle(96,64,iw - 32,ih - 32)
       ['#00008030','#00800030','#80000030'].tap do |colors|
         timesets = @notes.uniq.map{|t|[t,@notes.count(t)]}.sort_by(&:first).to_h
         timesets.each do |time,count|
           canvas.stroke colors.at( [count,colors.size].min - 1 )
           tx = sprx.call(time)
-          canvas.line(tx,64,tx,ih-32)
+          canvas.line(tx,ry,tx,rh)
         end
       end
+      canvas.stroke('black')
+      canvas.rectangle(rx,ry,rw,rh)
+      canvas.rectangle(rx,rh,rw,ch-32)
       #
     rescue => e
       $stderr.puts "#{e.class}: #{e.message}"
@@ -124,11 +183,67 @@ class BloomJewel::SkillUptime
     
     canvas.draw(img)
     ctime = [Time.now.to_f].pack('G').unpack('h*').first
-    img.write('results/%s.png')
+    ctime = '0000000000000000'
+    cfn = 'results/%s.png'%[ctime]
+    img.write(cfn)
+    puts JSON.dump({'file':cfn})
+    
+    self
   end
 end
 
 begin
-  
+  $scin = Scanner.new($stdin)
+  private
+  def read_cards
+    code = $scin.read(String)
+    case code
+    when 'm'
+      id = $scin.read(Integer)
+      id -= 1 if id.even?
+      rt = nil
+      BloomJewel.sqlite3(:master,:master) do |db|
+        att = {}
+        db.execute('select available_time_type,available_time_min from available_time_type').each do |(id,tmin)|
+          att.store(id,Rational(tmin,100))
+        end
+        db.get_first_row('select id,condition,available_time_type,10,skill_type from skill_data where id = ?',[id]).tap do |skill|
+          skill[2] = att[ skill[2] ]
+          skill[3] = $scin.read(Integer)
+          skill[5] = $scin.read(String)
+          rt = LiveCard.new(*skill)
+          rt&.skill!
+        end
+      end
+      fail ValueError, "ID #{id} not found" if rt.nil?
+      rt
+    else
+      LiveCard.new(*$scin.read(Integer,Integer,Float,Integer,Integer,String)).tap do |c| c.skill! end
+    end
+  end
+  def read_notes
+    $scin.read(Float)
+  end
+  def main
+    argv  = ARGV.dup
+    card  = $scin.read(Integer)
+    cards = card.times.map do read_cards end
+    mode  = $scin.read(String)
+    case mode
+    when 'f'
+      cset, diff = $scin.read(Integer,Integer)
+      chart = JSON.parse(File.read(BloomJewel.file(:radar,"s/%03d_%02d.json"%[cset,diff])))
+      notes = chart.select{|t|[1,2,3].include?(t['type'])}.map{|t|t['sec']}
+      slen  = chart.find{|t|t['type']==92}.fetch('sec')
+    else
+      combo = $scin.read(Integer)
+      notes = combo.times.map do read_notes end
+      slen  = $scin.read(Float)
+    end
+    sname = $scin.readline
+    BloomJewel::SkillUptime.new(cards,notes).tap do |upt|
+      upt.draw(slen,sname)
+    end
+  end
+  main
 end if $0 == __FILE__
-
